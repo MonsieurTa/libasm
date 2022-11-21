24e1ce1bd97e62c3363999e378a10f3afc436c13
William Ta <willt29000@gmail.com>
2022-11-21 15:50:56 +0100

    refactor: fix local variable size

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -6,16 +6,16 @@ section .text
 
 ft_atoi_base:
   xor rax, rax
-  sub rsp, 33
-  mov byte [rsp-33], 0x0 ; sign
-  mov [rsp-32], rdi ; str
-  mov [rsp-16], rsi ; str_base
+  sub rsp, 17
+  mov byte [rsp-17], 0x0 ; sign
+  mov [rsp-16], rdi ; str
+  mov [rsp-8], rsi ; str_base
 
   jmp trim_whitespace
 
 trim_whitespace:
   xor r10, r10
-  mov rcx, [rsp-32]
+  mov rcx, [rsp-16]
   mov r10b, [rcx]
 
   cmp r10b, 0x20 ; " "
@@ -34,58 +34,58 @@ trim_whitespace:
   jmp parse_plus_sign
 
 trim:
-  inc qword [rsp-32]
+  inc qword [rsp-16]
   jmp trim_whitespace
 
 parse_plus_sign:
   cmp r10b, 0x2b  ; "+"
   jne parse_neg_sign
 
-  inc qword [rsp-32]
+  inc qword [rsp-16]
   jmp parse_digit
 
 parse_neg_sign:
   cmp r10b, 0x2d  ; "-"
   jne parse_digit
 
-  mov byte [rsp-33], 1
-  inc qword [rsp-32]
+  mov byte [rsp-17], 1
+  inc qword [rsp-16]
   jmp parse_digit
 
 parse_digit:
   xor r10, r10
-  mov rcx, [rsp-32]
+  mov rcx, [rsp-16]
 
   mov r10b, [rcx]
   sub r10b, 0x30;     ; check if "0"
   cmp r10b, 0x0
   jl exit
-  cmp r10b, [rsp-16]
+  cmp r10b, [rsp-8]
   jl add_digit
 
   sub r10b, 0x7       ; check if "A"
   cmp r10b, 0xa
   jl exit
-  cmp r10b, [rsp-16]
+  cmp r10b, [rsp-8]
   jl add_digit
 
   sub r10b, 0x20      ; check if "a"
   cmp r10b, 0xa
   jl exit
-  cmp r10b, [rsp-16]
+  cmp r10b, [rsp-8]
   jl add_digit
 
   jmp exit
 
 add_digit:
-  mul qword [rsp-16]  ; multiply result by base
+  mul qword [rsp-8]  ; multiply result by base
   add rax, r10
   jmp redo
 
 redo:
   xor r10, r10
-  inc qword [rsp-32]  ; go to next character
-  mov r10, [rsp-32]
+  inc qword [rsp-16]  ; go to next character
+  mov r10, [rsp-16]
   cmp [r10], byte 0x0 ; exit if null character
   je exit
 
@@ -93,17 +93,17 @@ redo:
 
 apply_neg:
   xor r10, r10
-  mov r10b, [rsp-33]
+  mov r10b, [rsp-17]
   neg r10
   mul r10
 
-  mov byte [rsp-33], 0x0
+  mov byte [rsp-17], 0x0
   jmp exit
 
 exit:
-  cmp byte [rsp-33], 0x0  ; is it positive?
+  cmp byte [rsp-17], 0x0  ; is it positive?
   jne apply_neg
 
-  mov rdi, [rsp-32]
-  add rsp, 33
+  mov rdi, [rsp-16]
+  add rsp, 17
   ret
